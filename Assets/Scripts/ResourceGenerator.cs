715961db21d74fdc8c13581a181c0eb693478d1c
AndrushchenkoDmytro <d1mandruschenko@gmail.com>
2024-01-10 16:44:29 +0200

    Resource generation is based on the number of resources nearby

--- Assets/Scripts/ResourceGenerator.cs
+++ Assets/Scripts/ResourceGenerator.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ResourceGenerator : MonoBehaviour
 {
     [SerializeField] private Resource resourceType;
-    [SerializeField] private int resourcePerTimeInterval = 2;
+    [SerializeField] private int maxResourcesPerTimeInterval = 4;
+    [SerializeField] private int resourcesPerTimeInterval;
     [SerializeField] private float timeInterval = 2;
+    [SerializeField] private float detectRadius = 8f;
     private float currentTime = 0;
+    [SerializeField] private float resourceNodesAmount = 0;
+    private float requiredAmountNodes = 15;
+    [SerializeField] LayerMask resourcesLayer;
+
+    private void Start()
+    {
+        CheckResourceAmount();
+        if(resourceNodesAmount <= 1)
+        {
+            enabled = false;
+        }
+        else
+        {
+            float tmp = resourceNodesAmount / requiredAmountNodes * maxResourcesPerTimeInterval;
+            
+            resourcesPerTimeInterval = Mathf.Clamp(Mathf.RoundToInt(tmp), 0, maxResourcesPerTimeInterval);
+        }
+    }
 
     private void Update()
     {
@@ -18,7 +39,17 @@ public class ResourceGenerator : MonoBehaviour
         else
         {
             currentTime = 0;
-            ResourceManager.Instance.AddResource(resourceType, resourcePerTimeInterval);
+            ResourceManager.Instance.AddResource(resourceType, resourcesPerTimeInterval);
+        }
+    }
+
+    public void CheckResourceAmount()
+    {
+        Collider2D[] resourceNodes =  Physics2D.OverlapCircleAll(transform.position, detectRadius, resourcesLayer);
+        foreach(Collider2D node in resourceNodes)
+        {
+            if (node.gameObject.tag == resourceType.ToString())
+                resourceNodesAmount++;
         }
     }
 }
