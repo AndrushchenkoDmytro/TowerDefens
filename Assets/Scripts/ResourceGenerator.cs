63accb604e16c9bbbe9cb897bdc6557db471fbeb
AndrushchenkoDmytro <d1mandruschenko@gmail.com>
2024-01-13 10:22:30 +0200

    Add building overlay for production and ghostbuilding

--- Assets/Scripts/ResourceGenerator.cs
+++ Assets/Scripts/ResourceGenerator.cs
@@ -6,33 +6,39 @@ using UnityEngine.Rendering.Universal;
 public class ResourceGenerator : MonoBehaviour
 {
     [SerializeField] private Resource resourceType;
-    [SerializeField] private int maxResourcesPerTimeInterval = 4;
-    [SerializeField] private int resourcesPerTimeInterval;
-    [SerializeField] private float timeInterval = 2;
     [SerializeField] private float detectRadius = 8f;
+    private int resourcesPerTimeInterval = 1;
+    [SerializeField] private float generationTime = 0;
+    [SerializeField] private float minGerenationTime = 0.4f;
+    [SerializeField] private float maxGerenationTime = 0.8f;
     private float currentTime = 0;
     [SerializeField] private float resourceNodesAmount = 0;
     [SerializeField] private float requiredAmountNodes = 15;
     [SerializeField] LayerMask resourcesLayer;
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Start()
     {
         CheckResourceAmount();
-        if(resourceNodesAmount <= 1)
+        if(resourceNodesAmount < 1)
         {
-            enabled = false;
+            OffGenerator();
+
         }
         else
         {
-            float tmp = resourceNodesAmount / requiredAmountNodes * maxResourcesPerTimeInterval;
-            
-            resourcesPerTimeInterval = Mathf.Clamp(Mathf.RoundToInt(tmp), 0, maxResourcesPerTimeInterval);
+            CalculateGenerationSpeed();
         }
     }
 
     private void Update()
     {
-        if(currentTime < timeInterval)
+        if(currentTime < generationTime)
         {
             currentTime += Time.deltaTime;
         }
@@ -43,14 +49,52 @@ public class ResourceGenerator : MonoBehaviour
         }
     }
 
-    public void CheckResourceAmount()
+    public void CalculateGenerationSpeed()
     {
-        Collider2D[] resourceNodes =  Physics2D.OverlapCircleAll(transform.position, detectRadius, resourcesLayer);
-        foreach(Collider2D node in resourceNodes)
+        float tmp = minGerenationTime + (minGerenationTime - maxGerenationTime) * (resourceNodesAmount / requiredAmountNodes - 1);
+
+        generationTime = Mathf.Clamp(tmp, minGerenationTime, maxGerenationTime);
+    }
+
+    public float GetGenerationSpeed()
+    {
+        if (generationTime <= 0) return 0;
+        return resourcesPerTimeInterval / generationTime;
+    }
+
+    private void OffGenerator()
+    {
+        animator.enabled = false;
+        enabled = false;
+    }
+
+    private void CheckResourceAmount()
+    {
+        Collider2D[] resourceNodes = Physics2D.OverlapCircleAll(transform.position, detectRadius, resourcesLayer);
+        foreach (Collider2D node in resourceNodes)
         {
-            Debug.Log(node.tag+"1");
             if (node.gameObject.tag == resourceType.ToString())
                 resourceNodesAmount++;
         }
+    }
+
+    public int GetProductionPerformans(Vector3 checkPosition)
+    {
+        float resourceNodesAmount = 0;
+        Collider2D[] resourceNodes = Physics2D.OverlapCircleAll(checkPosition, detectRadius, resourcesLayer);
+        if (resourceNodes.Length == 0) return 0;
+        foreach (Collider2D node in resourceNodes)
+        {
+            if (node.gameObject.tag == resourceType.ToString())
+                resourceNodesAmount++;
+        }
+        resourceNodesAmount = Mathf.Clamp(resourceNodesAmount, 0, requiredAmountNodes);
+
+        return Mathf.RoundToInt(resourceNodesAmount / requiredAmountNodes * 100);
+    }
+
+    public Resource GetResourceType()
+    {
+        return resourceType;
     }
 }
